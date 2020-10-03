737d46361e08462c7acdc8393570092e18252033
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2020-10-03 19:52:10 +0200

    Добавьте файлы проекта.

---
+++ Два.Один/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Два.Один
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.Write("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                Console.Write("Введите y:");
+                double y = double.Parse(Console.ReadLine());
+                bool r = x >= Math.Abs(y) && y >= -1 && y <= 1 && x >= 0 && x <= 1;
+                Console.WriteLine(r);
+            
+        }
+    }
+}
